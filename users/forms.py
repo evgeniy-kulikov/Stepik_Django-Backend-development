3d0dcc092c02a42ae9f5690a3d8e87468e2b4670
evgeniy-kulikov <redstepway@gmail.com>
2023-04-02 18:19:45 +0300

    4.10 Собственные шаблоны и кастомизация формы

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,20 @@
 from django.contrib.auth.forms import AuthenticationForm
+from django import forms
 from users.models import User
 
 
 # Наследуемся от встроенного класса AuthenticationForm
 class UserLoginForm(AuthenticationForm):
+    # Переопределяем поля формы. Кастомизация формы.
+    username = forms.CharField(widget=forms.TextInput({
+    'class': 'form-control py-4',
+    'placeholder': 'Введите имя пользователя',
+    }))
+    password = forms.CharField(widget=forms.PasswordInput({
+    'class': 'form-control py-4',
+    'placeholder': 'Введите пароль',
+    }))
+
     class Meta:  # class Meta отвечает за выбор модели и отображаемых полей
         model = User
         fields = ('username', 'password')
