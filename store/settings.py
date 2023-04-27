a8fd29b42cbd5bd4d484c6f1865b67a85c19869b
evgeniy-kulikov <redstepway@gmail.com>
2023-04-27 10:44:48 +0300

    8.2 Работа с PostgreSQL

--- store/settings.py
+++ store/settings.py
@@ -81,10 +81,22 @@ WSGI_APPLICATION = 'store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        'NAME': 'store_db',
+        'USER': 'store_user',
+        'PASSWORD': 'store_password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
