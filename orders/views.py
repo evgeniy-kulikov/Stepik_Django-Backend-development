9ed1e6f4590658f68a42cf8e1497bdadc35167b0
evgeniy-kulikov <redstepway@gmail.com>
2023-05-05 10:53:27 +0300

    10.3 Создание OrderModel, OrderForm

--- orders/views.py
+++ orders/views.py
@@ -4,6 +4,7 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse, reverse_lazy
 
 from common.views import TitleMixin
+from orders.forms import OrderForm
 
 
 # Create your views here.
@@ -11,7 +12,7 @@ from common.views import TitleMixin
 
 class OrderCreateView(TitleMixin, CreateView):
     template_name = 'orders/order-create.html'
-    # form_class = OrderForm
+    form_class = OrderForm
     # success_url = reverse_lazy('orders:order_create')
     # title = 'Store - Оформление заказа'
 
